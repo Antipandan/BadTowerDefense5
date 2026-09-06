a3d5f1b4e17fe952da8430cfb8b2263874d3516e
Jacob <jacob@blota.se>
2026-09-06 16:30:40 +0200

    work more on object pool

--- Assets/Scripts/ObjectPools/ObjectPoolManager.cs
+++ Assets/Scripts/ObjectPools/ObjectPoolManager.cs
@@ -9,10 +9,50 @@ namespace ObjectPools
     {
         private static ObjectPoolManager instance;
         private readonly Dictionary<Object, Stack<Object>> pools = new Dictionary<Object, Stack<Object>>();
+        private readonly List<GameObject> hierarchyRepresentation = new List<GameObject>();
 
         public static ObjectPoolManager Instance
         {
             get => instance;
+        }
+
+        private void AddHierarchyRepresentation<TObjectToPool>(TObjectToPool objectToPool)
+        {
+            GameObject obj = Instantiate(new GameObject($"Pool: {objectToPool.ToString()}"), transform, true);
+            hierarchyRepresentation.Add(obj);
+        }
+
+        private void TryFindObjectOfType<TObject>(out int index)
+        {
+            for (int i = 0; i < hierarchyRepresentation.Count; i++)
+            {
+                if (hierarchyRepresentation[i].GetComponent<TObject>() != null)
+                {
+                    index = i;
+                }
+            }
+            index = -1;
+        }
+
+        private void MovePooledObjectIntoHierarchy<TObject>(TObject objectToPool) where TObject : Object
+        {
+            TryFindObjectOfType<TObject>(out int index);
+            if (index >= 0)
+            {
+                GameObject parent = hierarchyRepresentation[index];
+                if (parent is not null)
+                {
+                    // wtf
+                    GameObject obj = (GameObject)(Object)objectToPool;
+                    obj.transform.SetParent(parent.transform);
+                }
+            }
+        }
+
+        private void RemovePooledObjectFromHierarchy<TObject>(TObject objectToPool) where TObject : Object
+        {
+            GameObject obj = (GameObject)(Object)objectToPool;
+            obj.transform.SetParent(null);
         }
 
         public void AddNewPool<TObject>(TObject objectToPool, uint size = 1) where TObject : Object
@@ -20,7 +60,8 @@ namespace ObjectPools
             if (!pools.ContainsKey(objectToPool))
             {
                 pools.Add(objectToPool, new Stack<Object>((int)size));
-                
+                AddHierarchyRepresentation(objectToPool);
+                MovePooledObjectIntoHierarchy(objectToPool);
             }
             pools[objectToPool] = null;
             Debug.LogWarning($"cannot create this pool because a pool of type: {nameof(TObject)} already exists!)");
@@ -40,7 +81,8 @@ namespace ObjectPools
             if (objects.Count < shrinkAmount) RemovePool(objectToPool);
             for (int i = 0; i < shrinkAmount; i++)
             {
-                objects.Pop();
+                GameObject obj = (GameObject)(Object)objects.Pop();
+                Destroy(obj);
             }
         }
 
@@ -59,7 +101,7 @@ namespace ObjectPools
 
         public TObjectPool GetItemFromPool<TObjectPool>(TObjectPool objectPool) where TObjectPool : Object
         {
-            return pools[objectPool].Pop() as TObjectPool;
+            return pools[objectPool].Peek() as TObjectPool;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
